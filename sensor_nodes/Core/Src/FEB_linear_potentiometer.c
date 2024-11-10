f3e3fcddc43805fd5bfac661a2732744be36b670
Evan Robert <evanrobert@berkeley.edu>
2024-11-10 12:47:38 -0800

    converted raw values to mm

--- sensor_nodes/Core/Src/FEB_linear_potentiometer.c
+++ sensor_nodes/Core/Src/FEB_linear_potentiometer.c
@@ -3,8 +3,12 @@
 
 extern ADC_HandleTypeDef hadc1;
 extern UART_HandleTypeDef huart2;
+
 uint32_t linear_potentiometer_reading;
+float linear_potentiometer_value;
 char buf[164];
+float adc_resolution = 4095;
+float linear_potentiometer_length = 75; //mm
 
 
 uint32_t get_linear_potentiometer_reading(void){
@@ -17,7 +21,10 @@ uint32_t get_linear_potentiometer_reading(void){
 		linear_potentiometer_reading = (uint32_t)HAL_ADC_GetValue(&hadc1);
 	}
 
-	sprintf(buf, "Potentiometer Measurement: %u\r\n", (unsigned) linear_potentiometer_reading);
+	linear_potentiometer_value = linear_potentiometer_reading / adc_resolution * linear_potentiometer_length;
+
+
+	sprintf(buf, "Potentiometer Measurement: %d\r\n", (int) linear_potentiometer_value);
 	HAL_UART_Transmit(&huart2, (uint8_t *) buf, strlen(buf), HAL_MAX_DELAY);
 	return linear_potentiometer_reading;
 }
