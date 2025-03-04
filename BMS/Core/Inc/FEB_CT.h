f56c229e61d91f205c135f7afce560a762bfce1c
issssac <notissssac@gmail.com>
2025-03-04 01:45:47 -0800

    Write broken, all else is good

---
+++ BMS/Core/Inc/FEB_CT.h
@@ -0,0 +1,6 @@
+#ifndef INC_FEB_CT_H_
+#define INC_FEB_CT_H_
+
+//Cell temperature
+
+#endif /* INC_FEB_CT_H_ */
---
+++ BMS/Core/Inc/FEB_CV.h
@@ -0,0 +1,6 @@
+#ifndef INC_FEB_CV_H_
+#define INC_FEB_CV_H_
+
+//Cell voltage
+
+#endif /* INC_FEB_CV_H_ */
