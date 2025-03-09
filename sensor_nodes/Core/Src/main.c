581fb426be35186f2f95c73c8f5e97c8f83e3da3
rahilpasha <rahilpasha006@gmail.com>
2025-03-09 12:22:46 -0700

    update timer to 10Hz

--- sensor_nodes/Core/Src/main.c
+++ sensor_nodes/Core/Src/main.c
@@ -596,7 +596,7 @@ static void MX_TIM6_Init(void)
 
   /* USER CODE END TIM6_Init 1 */
   htim6.Instance = TIM6;
-  htim6.Init.Prescaler = 84-1;
+  htim6.Init.Prescaler = 840-1;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim6.Init.Period = 9999;
   htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
