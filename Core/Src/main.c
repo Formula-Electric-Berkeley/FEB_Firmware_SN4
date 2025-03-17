ea367d7b12928915a76198488a88bb0ee86f14c2
Rahil Pasha <31114117+rahilpasha@users.noreply.github.com>
2025-03-16 18:51:02 -0700

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -114,12 +114,12 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  FEB_Task_ADBMS();
+//	  FEB_Task_ADBMS();
 	  //FEB_Task_SM();
 	  //FEB_Task_Charge();
 	  //FEB_Task_Balance();
-	  //FEB_Task_IVT();
-	  //FEB_Task_CAN();
+	  FEB_Task_IVT();
+	  FEB_Task_CAN();
   }
   /* USER CODE END 3 */
 }
@@ -320,8 +320,8 @@ static void MX_USART2_UART_Init(void)
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -387,8 +387,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -408,7 +408,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   /* USER CODE BEGIN Callback 0 */
 
   /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM1) {
+  if (htim->Instance == TIM1)
+  {
     HAL_IncTick();
   }
   /* USER CODE BEGIN Callback 1 */
